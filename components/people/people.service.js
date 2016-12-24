3da556185a8da25dfa9b98c8d90b470438ea3e18
Zesk06 <zesk06@gmail.com>
2016-12-24 15:54:53 +0100

    remove unused code

--- components/people/people.service.js
+++ components/people/people.service.js
@@ -5,16 +5,6 @@ angular.module('hello').service('PeopleService', function($http) {
         return resp.data;
       });
     },
-
-    getPerson: function(id) {
-      function personMatchesParam(person) {
-        return person.id === id;
-      }
-
-      return service.getAllPeople().then(function (people) {
-        return people.find(personMatchesParam);
-      });
-    }
   };
 
   return service;
